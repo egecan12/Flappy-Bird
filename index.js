50b4f97b2d0c8522ff6653bfea9e8ce53a2b6258
Egecan Kahyaoglu <45043515+egecan12@users.noreply.github.com>
2019-07-04 00:49:04 -0400

    u

--- index.js
+++ index.js
@@ -1,40 +1,84 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+document.addEventListener("keydown", jump);
+
+function jump() {
+    bY = bY - 20;
+}
+
+var pipe = [];
+pipe[0] = {
+
+    x: 200,
+    y: 300
+}
+
+
+
+
+
 //my images loading
 
 var bg = new Image();
-var northPipe=new Image();
-var southPipe=new Image();
-var bird=new Image();
+var northPipe = new Image();
+var southPipe = new Image();
+var bird = new Image();
 var fg = new Image();
 
-bg.src="bb.png";
-northPipe.src= "north.png";
-southPipe.src= "south.png";
-bird.src="redbird.png";
-fg.src="https://github.com/sourabhv/FlapPyBird/blob/master/assets/sprites/base.png?raw=true"
+bg.src = "bb.png";
+northPipe.src = "north.png";
+southPipe.src = "south.png";
+bird.src = "redbird.png";
+fg.src = "https://github.com/sourabhv/FlapPyBird/blob/master/assets/sprites/base.png?raw=true"
 
 var gap = 85;
-var constant = southPipe.height-gap;
+var constant = southPipe.height - gap;
 
 //bird position
-var bX=0;
-var bY=300;
-var gravity =1 ;
+var bX = 0;
+var bY = 300;
+var gravity = 1;
 
 //draw
 
-function draw(){
+function draw() {
 
-ctx.drawImage(bg,0,0,600,800);
-ctx.drawImage(northPipe,200,0+constant);
-ctx.drawImage(southPipe,200,400);
-ctx.drawImage(bird,bX,bY);
-ctx.drawImage(fg,0,cvs.height-fg.height);
-bY += gravity;
-requestAnimationFrame(draw)
+    ctx.drawImage(bg, 0, 0, 600, 800);
+
+
+    for (var i = 0; i < pipe.length; i++) {
+      
+
+        ctx.drawImage(northPipe, pipe[i].x, pipe[i].y)
+
+        ctx.drawImage(southPipe, pipe[i].x, pipe[i].y+constant)
+
+        pipe[i].x--;
+
+        if(pipe[i].x == 112) {
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random().southPipe)
+            });
+
+        }
+
+    }
+    ctx.drawImage(bird, bX, bY);
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    bY += gravity;
+    requestAnimationFrame(draw)
+
+
+
+
+
 }
+
+
+
+
 
 
 draw();
